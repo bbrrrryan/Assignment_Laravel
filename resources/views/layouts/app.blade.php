99729b7fefd517ef80731357188090cdf1daae64
LiewZiLi <liewzili418@gmail.com>
2025-12-13 03:41:13 +0800

    Change design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -349,11 +349,11 @@
                     @endif
                 @else
                     <li><a href="{{ route('home') }}">Home</a></li>
-                    <li><a href="{{ route('facilities.index') }}">Facilities</a></li>
-                    <li><a href="{{ route('bookings.index') }}">Bookings</a></li>
-                    <li><a href="{{ route('notifications.index') }}">Notifications</a></li>
-                    <li><a href="{{ route('loyalty.index') }}">Loyalty</a></li>
-                    <li><a href="{{ route('feedbacks.index') }}">Feedback</a></li>
+                <li><a href="{{ route('facilities.index') }}">Facilities</a></li>
+                <li><a href="{{ route('bookings.index') }}">Bookings</a></li>
+                <li><a href="{{ route('notifications.index') }}">Notifications</a></li>
+                <li><a href="{{ route('loyalty.index') }}">Loyalty</a></li>
+                <li><a href="{{ route('feedbacks.index') }}">Feedback</a></li>
                 @endauth
                 <li id="authLinks">
                     @auth
